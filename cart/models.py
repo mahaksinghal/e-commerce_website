5f60ef2387abc412331a6dbac3a2ef558d1b485f
mahak <mahaksinghal567@gmail.com>
2024-09-04 14:02:32 +0530

    cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateField(auto_now_add=True)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    quantity = models.PositiveIntegerField(default=1)
+    address = models.TextField(help_text = "Enter Delivery Address")
+    ordered_at = models.DateField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+    is_delivered = models.BooleanField(default=False)
