778682a844768bf7ec2502c1bd9a53b44f98a47c
mahak <mahaksinghal567@gmail.com>
2024-10-22 15:15:02 +0530

    changes

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
 from main import views as mv
 from cart import views as cv
 
